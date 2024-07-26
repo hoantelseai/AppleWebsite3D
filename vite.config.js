14284f81aa6a70e363a37b5bfbab2e47d9c25fb4
hoantelseai <108644551+hoantelseai@users.noreply.github.com>
2024-07-26 16:39:02 +0900

    add: deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), sentryVitePlugin({
+  plugins: [react(),sentryVitePlugin({
     org: "jsm-y2c",
-    project: "javascript-react"
+    project: "javascript-react",
+    
   })],
-
+ base: "/Apple_Website_3D/",
   build: {
     sourcemap: true
   }
