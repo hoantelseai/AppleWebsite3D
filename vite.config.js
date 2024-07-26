c1c539b698ef485be320806cb12b74c7255c1f7e
hoantelseai <108644551+hoantelseai@users.noreply.github.com>
2024-07-26 17:39:36 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
-import { sentryVitePlugin } from "@sentry/vite-plugin";
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { sentryVitePlugin } from "@sentry/vite-plugin";
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 // export default defineConfig({
@@ -16,6 +16,15 @@ import react from '@vitejs/plugin-react'
 //   }
 // })
 
+// export default defineConfig({
+//   base: "/AppleWebsite3D/",
+//   plugins: [react()],
+// })
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
 export default defineConfig({
   // base: "/AppleWebsite3D/",
   plugins: [react()],
