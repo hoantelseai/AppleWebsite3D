aba9e7a11db836b5192d439f356198f26613b6a5
hoantelseai <108644551+hoantelseai@users.noreply.github.com>
2024-07-29 09:13:21 +0900

    deploy

--- vite.config.js
+++ vite.config.js
@@ -1,31 +1,31 @@
-// import { sentryVitePlugin } from "@sentry/vite-plugin";
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
+import { sentryVitePlugin } from "@sentry/vite-plugin";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-// export default defineConfig({
-//   base: "/Apple_Website_3D/",
-//   plugins: [react()
-//     ,sentryVitePlugin({
-//     org: "jsm-y2c",
-//     project: "javascript-react",
-//   })
-// ],
-//   build: {
-//     sourcemap: true
-//   }
-// })
+export default defineConfig({
+  // base: "/Apple_Website_3D/",
+  plugins: [react()
+    ,sentryVitePlugin({
+    org: "jsm-y2c",
+    project: "javascript-react",
+  })
+],
+  build: {
+    sourcemap: true
+  }
+})
 
 // export default defineConfig({
 //   base: "/AppleWebsite3D/",
 //   plugins: [react()],
 // })
 
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  // base: "/AppleWebsite3D/",
-  plugins: [react()],
-})
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   base: "/AppleWebsite3D/",
+//   plugins: [react()],
+// })
